df56042d5833220c738ae8cf9bda3af75b6c10a2
Ray Mayo <ormillada.jr@gmail.com>
2022-09-19 18:16:00 +0800

    fixed fullpage and added navigation on the left

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 new fullpage('#fullpage', {
     //options here
     autoScrolling: true,
-    scrollHorizontally: true
+    scrollHorizontally: false,
+
+    onLeave: function (origin, destination, direction) {
+        activateNavItem($('#icons').find('li').eq(destination.index));
+    },
+    afterRender: function () {
+        activateNavItem($('#icons').find('li').eq($('.section.active').index()))
+    }
 });
+
+$('.navIcon').click(function () {
+    var destination = $(this).closest('li');
+    fullpage_api.moveTo(destination.index() + 1);
+});
+
+function activateNavItem(item) {
+    item.addClass('active').siblings().removeClass('active');
+}
+
+window.addEventListener('load', ()=>{
+    const loader = document.querySelector('.loader');
+    loader.className += ' hidden';
+})
