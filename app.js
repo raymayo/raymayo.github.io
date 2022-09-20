6030668bd8aa5f2e68fc7bad44156803b14ea47c
Ray Mayo <ormillada.jr@gmail.com>
2022-09-20 18:09:15 +0800

    added animation

--- app.js
+++ app.js
@@ -20,7 +20,35 @@ function activateNavItem(item) {
     item.addClass('active').siblings().removeClass('active');
 }
 
-window.addEventListener('load', ()=>{
-    const loader = document.querySelector('.loader');
-    loader.className += ' hidden';
-})
+//PRELOADER
+// window.addEventListener('load', ()=>{
+//     const loader = document.querySelector('.loader');
+//     loader.className += ' hidden';
+// })
+
+
+//PARALLAX EFFECT
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed)/100
+        const y = (window.innerHeight - e.pageY * speed)/100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+// document.addEventListener("mousemove", parallax);
+
+// function parallax(e) {
+//     this.querySelectorAll('.layer').forEach(layer => {
+//         const speed = layer.getAttribute('data-speed')
+
+//         const x = (window.innerWidth - e.pageX * speed) / 100
+//         const y = (window.innerWidth - e.pageY * speed) / 100
+
+//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+//     })
+// }
