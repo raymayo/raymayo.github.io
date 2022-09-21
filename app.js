2ebe8e25e204c9f91be721019266f55ca30d437a
Ray Mayo <ormillada.jr@gmail.com>
2022-09-21 17:50:18 +0800

    added button

--- app.js
+++ app.js
@@ -40,15 +40,20 @@ function parallax(e) {
     })
 }
 
-// document.addEventListener("mousemove", parallax);
 
-// function parallax(e) {
-//     this.querySelectorAll('.layer').forEach(layer => {
-//         const speed = layer.getAttribute('data-speed')
 
-//         const x = (window.innerWidth - e.pageX * speed) / 100
-//         const y = (window.innerWidth - e.pageY * speed) / 100
+        const images = {
+            projOne: { image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" },
+            projTwo: { image: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" }
+        }
 
-//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-//     })
-// }
+        let page = document.getElementById('fullpage');
+
+        page.children[0].children[4].style.backgroundImage = `url(${images.projOne.image})`
+        page.children[1].children[4].style.backgroundImage = `url(${images.projTwo.image})`
+
+
+    
+
+
+
